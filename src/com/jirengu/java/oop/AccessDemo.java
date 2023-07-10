a8e9b83ddb257cfd7e765a146f5ded69022b9f78
alicifes <635023574@qq.com>
2023-07-10 20:40:27 +0800

    update

---
+++ src/com/jirengu/java/oop/AccessDemo.java
@@ -0,0 +1,8 @@
+package com.jirengu.java.oop;
+
+public class AccessDemo {
+    public static void main(String[] args) {
+        //AuthControl authControl = new AuthControl(10, "xixiao");
+
+    }
+}
