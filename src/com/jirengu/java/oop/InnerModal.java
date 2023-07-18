7d97c6fe873410b9b0e4532103b15ab936e2b900
alicifes <635023574@qq.com>
2023-07-18 20:43:40 +0800

    update

---
+++ src/com/jirengu/java/oop/InnerModal.java
@@ -0,0 +1,19 @@
+package com.jirengu.java.oop;
+
+public class InnerModal {
+    //jvm 内存结构 (重点)
+
+    //jmm java memory modal java内存模型
+    //java对象模型
+
+    //java内存模型 主要分为两块 栈stack  以及  堆heap 方法区
+
+    //基本类型：存在stack，直接存储
+    //引用类型：对象存在stack区,本质上是存的地址的索引,指向heap，heap存储对象
+
+    //方法区  线程池（共享）类的信息 属性 方法 常量池 类  大小
+
+    //堆 保存new出来的对象不联系  但是栈是一个连续的东西(线程)LIFO
+    public static void main(String[] args) {
+    }
+}
