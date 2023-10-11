93fc5433c99b6b31e2ad3c616c005f23f9bed9db
alicifes <635023574@qq.com>
2023-10-11 10:28:47 +0800

    add

--- src/com/jirengu/java/collection/info/Info.java
+++ src/com/jirengu/java/collection/info/Info.java
@@ -16,14 +16,11 @@ public class Info {
     //在 java 中，设计的时候是把 collect 接口和 map 接口分开的
     //collect 中有 set (hashSet) 和 list(ArrayList) 以及 map(hashMap)
 
-
     //在今后的开发过程中，请不要使用 stack 和 vector 推荐使用 queue
     //在操作队列的过程中请使用新的方法 pick 和 offer
 
-
     //collection是属于单值接口，而对于 Map
     //treeMap 是有序的，hashMap 是无序的
-
     //自顶向下
     //abstract class AbstractCollection<E> implements Collection<E>
 }
