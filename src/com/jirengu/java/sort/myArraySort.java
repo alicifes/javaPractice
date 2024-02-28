167f2afb06f825a459693064d47394146da3f007
alicife <yuxiang.chen@schenckprocess.com>
2024-02-28 16:46:32 +0800

    common update

--- src/com/jirengu/java/sort/myArraySort.java
+++ src/com/jirengu/java/sort/myArraySort.java
@@ -9,21 +9,24 @@ public class myArraySort {
     public static void main(String[] args) {
         int [] array = {1,2,3,4,5,6,7,8,9,10};
         for(int num : array){
-            System.out.println("past" + num);
+            //System.out.println("past" + num);
         }
         Arrays.sort(array);
         for (int num : array){
-            System.out.println("cur" + num);
+            //System.out.println("cur" + num);
         }
         Student[] students = new Student[5];
-        students[0] = new Student(1,"张三",100);
-        students[1] = new Student(2,"李四",99);
+        students[0] = new Student(3,"张三",100);
+        students[1] = new Student(1,"李四",99);
         students[2] = new Student(3,"王五",98);
         students[3] = new Student(4,"赵六",97);
         students[4] = new Student(5,"孙七",96);
         for(Student item : students){
             System.out.println("pre"+item.id);
         }
-        students.sort();
+        Arrays.sort(students);
+        for(Student item : students){
+            System.out.println("cur"+item.id);
+        }
     }
 }
